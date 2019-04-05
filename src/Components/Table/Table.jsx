4175d982cf241413af38c68d4885678ea1b28c3a
Piyush Johnson <piyushjohnson1998@gmail.com>
2019-04-05 16:51:09 +0530

    Modified Home Page

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Table, Col, Row } from 'antd'
+import {Row, List } from 'antd'
+import { Link } from 'react-router-dom'
 import uuidv4 from 'uuid/v4'
 import Api from '../../Services/dataService'
 
@@ -7,7 +8,8 @@ export default class ListTable extends Component {
   constructor () {
     super()
     this.state = {
-      data: []
+      data: [],
+      isLoading: true
     }
   }
 
@@ -17,35 +19,41 @@ export default class ListTable extends Component {
           this.setState({
             data: info.records.map(movie => ({
               key: uuidv4(),
+              id: movie.id,
               title: movie.fields.title,
               description: movie.fields.description,
+              image: movie.fields.image,
               rating: movie.fields.rating,
               year: movie.fields.year
-            }))
+            })),
+            isLoading: false
           })
         })
   }
 
   render () {
-    const columns = [{
-      title: 'Title',
-      dataIndex: 'title'
-    }, {
-      title: 'Description',
-      dataIndex: 'description'
-    }, {
-      title: 'Rating',
-      dataIndex: 'rating'
-    }, {
-      title: 'Year',
-      dataIndex: 'year'
-    }]
 
     return (
       <Row>
-        <Col>
-          <Table columns={columns} dataSource={this.state.data} />
-        </Col>
+        <List
+          itemLayout="vertical"
+          size="small"
+          dataSource={this.state.data}
+          loading={this.state.isLoading}
+          footer={<div><b>OceanOfMovies</b> 2019-20</div>}
+          renderItem={item => (
+            <List.Item
+              key={item.title}
+              extra={<img width={272} alt="logo" src={item.image} />}
+            >
+            <List.Item.Meta
+              title={<Link to={`/movie/${item.id}`}>{item.title}</Link>}
+              description={item.rating}
+            />
+            {item.description}
+          </List.Item>
+        )}
+      />
       </Row>
     )
   }
